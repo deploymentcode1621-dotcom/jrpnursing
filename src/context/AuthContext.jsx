44b33db6fa274ca0b245b8de2590eefec11691c0
AdityaRaut <adityaraut6029@gmail.com>
2026-04-27 22:39:52 +0530

    code setup

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from 'react'
+// import { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
 const AuthContext = createContext()
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,13 @@
+import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import Home from './pages/Home'
-import About from './pages/About'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        
       </Route>
     </Routes>
   )
